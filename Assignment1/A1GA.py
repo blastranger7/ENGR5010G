11a58e9176037ec05eb869da2c06052cf7a8ab85
blastranger7 <limeejwalker@gmail.com>
2026-07-09 12:03:06 -0400

    Mutation Implementation

--- Assignment1/A1GA.py
+++ Assignment1/A1GA.py
@@ -18,7 +18,7 @@ def initPop(pop_size):
     return x,y,fit
 
 def tournamentSelection(k,x,y,fit):
-    best_x = np.empty(np.size(fit))
+    best_x = np.empty(np.size(fit)) 
     best_y = np.empty(np.size(fit))
     best_fit = np.empty(np.size(fit))
 
@@ -32,7 +32,7 @@ def tournamentSelection(k,x,y,fit):
                 opt_p = p
         best_x[jj] = x[opt_p]
         best_y[jj] = y[opt_p]
-        best_fit[jj] = y[opt_fit]
+        best_fit[jj] = opt_fit
 
     return best_x, best_y, best_fit
 
@@ -42,17 +42,23 @@ def crossover(x,y):
     child_y = np.append(child_y, y[0:np.size(y)/2])
     return child_x, child_y
 
-def mutate(x, y):
-    child_x = x
-    child_y = y
+def mutate(x, y, rate):
+    fit = np.empty(np.size(x))
+    for ii in range(np.size(x)):
+        if random.rand() < rate: #randomly swap chromosomes with a certain probability
+           x_val = x[ii]
+           x[ii] = y[ii]
+           y[ii] = x_val
+           fit[ii] = fitness(x[ii],y[ii])
     fit = 0
-    return child_x, child_y, fit
+    return x, y, fit
 
 def main():
     #initialize variables
     pop_size = 100
     num_iterations = 100
     k = 5
+    mutation_rate = 0.05
 
     #initialize plotting matricies
     avg_fitness = np.empty(pop_size)
@@ -62,7 +68,7 @@ def main():
     for ii in range(num_iterations):
         best_x, best_y, best_fit = tournamentSelection(k, x, y, fit)
         child_x, child_y = crossover(best_x, best_y)
-        child_x, child_y, child_fit = mutate(child_x, child_y)
+        child_x, child_y, child_fit = mutate(child_x, child_y, mutation_rate)
         x = child_x
         y = child_y
         fit = child_fit
