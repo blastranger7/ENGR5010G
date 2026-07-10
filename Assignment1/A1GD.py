473946ad9158500b8d8a86a8f48940f0e74f5c4c
blastranger7 <limeejwalker@gmail.com>
2026-07-10 14:41:18 -0400

    Added penalty for GD algorithm

--- Assignment1/A1GD.py
+++ Assignment1/A1GD.py
@@ -8,19 +8,20 @@ def project(x,y):
     return x,y
 
 def main():
-    x = 2
-    y = 2
+    x = 0.25
+    y = 0.25
     alpha = 0.001
-    num_iterations = 20
+    num_iterations = 100
+    mu = 0.5
+
     z = np.empty(num_iterations)
     for ii in range(num_iterations):
         zx = 2*x + 20*math.pi*math.sin(2*math.pi*x)
         zy = 2*y + 20*math.pi*math.sin(2*math.pi*y)
         x = x - alpha*zx
         y = y - alpha*zy
-        z[ii] = 20 + (x**2 - 10*math.cos(2*math.pi*x)) + (y**2 - 10*math.cos(2*math.pi*y)) 
-
-        #Needs Projection
+        z[ii] = (20 + (x**2 - 10*math.cos(2*math.pi*x)) + (y**2 - 10*math.cos(2*math.pi*y)) #Origional Loss Function
+                 + mu*(max(0, 1-x-y)**3 + max(0, x**2 + y**2 -20)**2)) #Inequality Penalty Functions (Need Adjustment)
 
     print(f"Best fitness is {z[-1]} at ({x}, {y}))")
     plt.plot(z)
